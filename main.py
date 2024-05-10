a3b4ffbdb5ad23aef2d1f70544e9f05585384373
Dzeratt <92248389+Dzeratt@users.noreply.github.com>
2024-05-11 01:34:35 +0700

    LAB6

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import asyncio
 
 import psycopg2
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.types import BotCommand, BotCommandScopeDefault
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import *
 import aiohttp
-
-from datetime import datetime
 
 import logging
 
@@ -35,9 +33,9 @@ def is_float(value):
 def dbConnect():
     conn = psycopg2.connect(
         host='127.0.0.1',
-        database='RGZ_PAD',
-        user='pader',
-        password='rgzpas')
+        database='tgbot',
+        user='tgbotadmin',
+        password='321')
 
     return conn
 
@@ -47,204 +45,221 @@ def dbClose(cursor, connection):
     connection.close()
 
 
-kb_user = [
-    [types.KeyboardButton(text='РАСХОД'), types.KeyboardButton(text='ДОХОД')]
+async def send_post_request(url, json_data):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=json_data) as response:
+            data = await response.json()
+            return data
+
+
+async def send_get_request(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.json()
+            return data
+
+
+kb = [
+    [types.KeyboardButton(text='Добавить валюту')],
+    [types.KeyboardButton(text='Удалить валюту')],
+    [types.KeyboardButton(text='Изменить курс валюты')]
 ]
-keyboard_user = types.ReplyKeyboardMarkup(keyboard=kb_user, resize_keyboard=True)
+keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
 
-
-kb_currency = [
-    [types.KeyboardButton(text='USD'), types.KeyboardButton(text='EUR'), types.KeyboardButton(text='RUB')]
-]
-keyboard_currency = types.ReplyKeyboardMarkup(keyboard=kb_currency, resize_keyboard=True)
-
-
-kb_sort = [
-    [types.KeyboardButton(text='ПО ВОЗРАСТАНИЮ'), types.KeyboardButton(text='ПО УБЫВАНИЮ')]
-]
-keyboard_sort = types.ReplyKeyboardMarkup(keyboard=kb_sort, resize_keyboard=True)
+url = 'http://127.0.0.1:5003/check_admin'
 
 
 class states(StatesGroup):
-    reg_get = State()
-    get_amount = State()
-    get_date = State()
-    save_oper = State()
-    get_value = State()
-    show = State()
+    add_name = State()
+    add_rate = State()
+    del_name = State()
+    edit_name = State()
+    edit_rate = State()
+    convert_name = State()
+    convert_rate = State()
 
 
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await message.answer(
-        'Привет, я Oper_Bot.\n'
-        'Воспользуйся меню или используй:\n'
-        '/reg чтобы зарегистрироваться;\n'
-        '/add_operation чтобы добавить операцию;\n'
-        '/operations чтобы посмотреть операции'
+        'Привет, я DeepBot.'
     )
 
 
-@dp.message(Command('reg'))
-async def reg(message: types.Message, state: FSMContext):
-    conn = dbConnect()
-    cur = conn.cursor()
+@dp.message(Command('manage_currency'))
+async def manage_currency(message: types.Message):
     id = message.chat.id
-    cur.execute('SELECT * FROM users WHERE id = %s', (id, ))
-    if cur.fetchone() is None:
-        await state.update_data(id=id)
-        await message.answer('Введите логин')
-        await state.set_state(states.reg_get)
-    else:
-        await message.answer('Вы уже зарегестрированы')
+    response_data = await send_post_request(url, {'id': id})
+    if 'error' in response_data:
+        await message.answer('Нет доступа.')
         return
+    else:
+        await message.answer('Выберите действие.', reply_markup=keyboard)
 
 
-@dp.message(states.reg_get)
-async def reg(message: types.Message, state: FSMContext):
-    data = await state.get_data()
-    id = data.get('id')
+@dp.message(F.text == 'Добавить валюту')
+async def add_name(message: types.Message, state: FSMContext):
+    id = message.chat.id
+    response_data = await send_post_request(url, {'id': id})
+    if 'error' in response_data:
+        await message.answer('Нет доступа.')
+        return
+    else:
+        await message.answer('Введите название валюты.')
+        await state.set_state(states.add_name)
+
+
+@dp.message(states.add_name)
+async def add_rate(message: types.Message, state: FSMContext):
     conn = dbConnect()
     cur = conn.cursor()
-    cur.execute('INSERT INTO users (id, name) VALUES (%s, %s)', (id, message.text))
-    conn.commit()
+    cur.execute(f'SELECT currency_name FROM currencies WHERE currency_name = %s;', (message.text.strip().upper(),))
+
+    if cur.fetchone():
+        await message.answer('Данная валюта уже существует.')
+        await state.set_state(None)
+        return
 
     dbClose(cur, conn)
 
-    await message.answer('Вы успешно зарегестрировались')
-    await state.set_state(None)
+    await state.update_data(name=message.text)
+    await message.answer('Введите курс рубля к единице данной валюты.')
+
+    await state.set_state(states.add_rate)
 
 
-@dp.message(Command('add_operation'))
-async def add_operation(message: types.Message, state: FSMContext):
-    conn = dbConnect()
-    cur = conn.cursor()
-    id = message.chat.id
-    cur.execute('SELECT * FROM users WHERE id = %s', (id, ))
-    if cur.fetchone() is None:
-        await state.update_data(id=id)
-        await message.answer('Зарегистрируйтесь')
-        return
-    else:
-        await message.answer('Какая операция', reply_markup=keyboard_user)
-        await state.set_state(states.get_amount)
-
-
-@dp.message(states.get_amount)
-async def get_amount(message: types.Message, state: FSMContext):
-    await state.update_data(operation=message.text.lower())
-    await message.answer('Введите сумму операции в рублях')
-    await state.set_state(states.get_date)
-
-
-@dp.message(states.get_date)
-async def get_date(message: types.Message, state: FSMContext):
-    sum = message.text
-
-    try:
-        float(sum)
-    except ValueError:
-        await message.answer('Неверное значение')
-        return
-
-    sum = float(sum)
-
-    if sum <= 0:
-        return
-
-    await state.update_data(amount=sum)
-
-    await message.answer('Введите дату операции (ГГГГ-ММ-ДД)')
-    await state.set_state(states.save_oper)
-
-
-@dp.message(states.save_oper)
-async def save_oper(message: types.Message, state: FSMContext):
-    date = message.text
-    try:
-        datetime.strptime(date, '%Y-%m-%d')
-    except ValueError:
-        await message.answer('Дата введена неверно')
-        return
-    date = datetime.strptime(date, '%Y-%m-%d')
-    if date >= datetime.now():
-        await message.answer('Дата не может быть больше сегодняшней')
-        return
+@dp.message(states.add_rate)
+async def add_currency(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    amount = data.get('amount')
-    operation = data.get('operation')
-    conn = dbConnect()
-    cur = conn.cursor()
-    cur.execute('INSERT INTO operations (dates, sum, chat_id, type_operations) VALUES (%s, %s, %s, %s)',
-                (date, amount, message.chat.id, operation, ))
-    conn.commit()
-    dbClose(cur, conn)
+    name = data.get('name')
 
-    await message.answer('Операция успешно добавлена')
+    url = 'http://127.0.0.1:5001/load'
+    json_data = {'name': name, 'rate': message.text}
+    response_data = await send_post_request(url, json_data)
+
+    if 'error' in response_data:
+        await message.answer(str(response_data['error']))
+    else:
+        await message.answer(str(response_data['message']))
+
     await state.set_state(None)
 
 
-@dp.message(Command('operations'))
-async def operations(message: types.Message, state: FSMContext):
-    conn = dbConnect()
-    cur = conn.cursor()
+@dp.message(F.text == 'Удалить валюту')
+async def del_name(message: types.Message, state: FSMContext):
     id = message.chat.id
-    cur.execute('SELECT * FROM users WHERE id = %s', (id,))
-    if cur.fetchone() is None:
-        await state.update_data(id=id)
-        await message.answer('Зарегистрируйтесь')
+    response_data = await send_post_request(url, {'id': id})
+    if 'error' in response_data:
+        await message.answer('Нет доступа.')
         return
     else:
-        await message.answer('Каким методом', reply_markup=keyboard_sort)
-        await state.set_state(states.get_value)
+        await message.answer('Введите название валюты.')
+        await state.set_state(states.del_name)
 
 
-@dp.message(states.get_value)
-async def currency(message: types.Message, state: FSMContext):
-    await state.update_data(method=message.text)
-    await message.answer('В какой валюте', reply_markup=keyboard_currency)
-    await state.set_state(states.show)
+@dp.message(states.del_name)
+async def del_currency(message: types.Message, state: FSMContext):
+    url = 'http://127.0.0.1:5001/delete'
+    json_data = {'name': message.text.upper().strip()}
+    response_data = await send_post_request(url, json_data)
 
-
-@dp.message(states.show)
-async def show_result(message: types.Message, state: FSMContext):
-    value = message.text.strip().upper()
-    id=message.chat.id
-    conn = dbConnect()
-    cur = conn.cursor()
-    data = await state.get_data()
-    method = data.get('method')
-    if method == 'ПО ВОЗРАСТАНИЮ':
-        cur.execute("SELECT * FROM operations WHERE chat_id = '%s' ORDER BY sum ASC", (id,))
+    if 'error' in response_data:
+        await message.answer(str(response_data['error']))
     else:
-        cur.execute("SELECT * FROM operations WHERE chat_id = '%s' ORDER BY sum DESC", (id,))
-    currencies = cur.fetchall()
-    if value == 'USD' or value == 'EUR':
-        url = 'http://195.58.54.159:8000/rate?currency='+value
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                response = await response.json()
-        result = ""
-        for currency in currencies:
-            sum = currency[2]
-            result_sum = float(sum)/float(response['rate'])
-            result_sum = round(result_sum, 2)
-            result += f"Дата: {currency[1]}; Сумма: {result_sum} {value}; Операция: {currency[4]}\n"
-        await message.answer(result)
-    else:
-        result = ''
-        for currency in currencies:
-            result += f"Дата: {currency[1]}; Сумма: {currency[2]}; Операция: {currency[4]}\n"
-        await message.answer(result)
-    dbClose(cur, conn)
+        await message.answer(str(response_data['message']))
+
     await state.set_state(None)
 
+
+@dp.message(F.text == 'Изменить курс валюты')
+async def edit_name(message: types.Message, state: FSMContext):
+    id = message.chat.id
+    response_data = await send_post_request(url, {'id': id})
+    if 'error' in response_data:
+        await message.answer('Нет доступа.')
+        return
+    else:
+        await message.answer('Введите название валюты.')
+        await state.set_state(states.edit_name)
+
+
+@dp.message(states.edit_name)
+async def edit_rate(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer('Введите курс рубля к единице данной валюты.')
+
+    await state.set_state(states.edit_rate)
+
+
+@dp.message(states.edit_rate)
+async def edit_currency(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    name = data.get('name')
+
+    url = 'http://127.0.0.1:5001/update_currency'
+    json_data = {'name': name, 'rate': message.text}
+    response_data = await send_post_request(url, json_data)
+
+    if 'error' in response_data:
+        await message.answer(str(response_data['error']))
+    else:
+        await message.answer(str(response_data['message']))
+
+    await state.set_state(None)
+
+
+@dp.message(Command('get_currencies'))
+async def get_currencies(message: types.Message):
+    url = 'http://127.0.0.1:5002/currencies'
+    response_data = await send_get_request(url)
+
+    currencies = response_data.get('currencies')
+    message_text = "Список валют:\n"
+    for currency in currencies:
+        currency_name = currency.get('currency_name')
+        rate = currency.get('rate')
+        message_text += f"Валюта: {currency_name}, Курс: {rate}\n"
+    await message.answer(message_text)
+
+
+@dp.message(Command('convert'))
+async def convert_name(message: types.Message, state: FSMContext):
+    await message.answer('Введите название валюты.')
+    await state.set_state(states.convert_name)
+
+
+@dp.message(states.convert_name)
+async def convert_rate(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer('Введите сумму.')
+    await state.set_state(states.convert_rate)
+
+
+@dp.message(states.convert_rate)
+async def convert_currency(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    name = data.get('name')
+
+    url = 'http://127.0.0.1:5002/convert?currency_name=' + name + '&amount=' + message.text
+    response_data = await send_get_request(url)
+
+    if 'error' in response_data:
+        await message.answer(str(response_data['error']))
+    else:
+        await message.answer(str(response_data['converted_amount']))
+
+    await state.set_state(None)
+
+
+@dp.message()
+async def any_commands(message: types.Message):
+    await message.answer('Такой команды нет, мне нечего ответить.')
 
 comands = [
     BotCommand(command='/start', description='Начать работу'),
-    BotCommand(command='/reg', description='Регистрация'),
-    BotCommand(command='/add_operation', description='Добавить операцию'),
-    BotCommand(command='/operations', description='Все операции'),
+    BotCommand(command='/manage_currency', description='Управление'),
+    BotCommand(command='/get_currencies', description='Все валюты'),
+    BotCommand(command='/convert', description='Конвертировать'),
 ]
 
 
